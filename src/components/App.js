981059d7436dbc98b27e507099160454afff1343
gonewrong2910 <73762000+gonewrong2910@users.noreply.github.com>
2022-10-02 10:47:08 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,52 +1,61 @@
-import React ,{useState , useEffect} from "react";
+import React from "react";
+import { useState,useEffect } from "react";
 import "../styles/App.css";
 
 var colors = [
-  '#16a085',
-  '#27ae60',
-  '#2c3e50',
-  '#f39c12',
-  '#e74c3c',
-  '#9b59b6',
-  '#FB6964',
-  '#342224',
-  '#472E32',
-  '#BDBB99',
-  '#77B1A9',
-  '#73A857'
+  "#16a085",
+  "#27ae60",
+  "#2c3e50",
+  "#f39c12",
+  "#e74c3c",
+  "#9b59b6",
+  "#FB6964",
+  "#342224",
+  "#472E32",
+  "#BDBB99",
+  "#77B1A9",
+  "#73A857"
 ];
 
- 
 const App = () => {
-  const [quotes , setQuotes] = useState([])
-  const [author , setAuthor] = useState([])
-  let color = parseInt(Math.random()*12);
-     async function generateQuotes(){
-      
-      let res = await fetch('https://api.quotable.io/random');
-      let data = await res.json();
-      setQuotes(data.content)
-      setAuthor(data.author)
-      }
-     
-     useEffect(()=>{
-      document.body.style.backgroundColor = colors[color]
-     })
-    return (
-      <div id="main">
-        <div id="wrapper">
-        <div className="quote-text">
-            <h2>{quotes}</h2>
-        </div>
-        <div className="quote-author">
-          { author}
-        </div>
-        <div className="buttons">
-          <button onClick={generateQuotes} id='new-quote' className="button">Click</button>
+  const [text, setText] = useState("");
+  const [author, setAuthor] = useState("");
+  
+  useEffect(()=>{
+  fetch("https://api.quotable.io/random")
+      .then((res) => res.json())
+      .then((data) => {
+        setText(data.content);
+        setAuthor(data.author);})}
+  ,[])
+
+
+  const fetchQ = () => {
+    fetch("https://api.quotable.io/random")
+      .then((res) => res.json())
+      .then((data) => {
+        setText(data.content);
+        setAuthor(data.author);
+        document.getElementsByTagName("body")[0].style.backgroundColor =
+          colors[parseInt(Math.random() * 10)];
+      });
+  };
+
+  return (
+    <div id="main">
+      <div id="wrapper">
+        <div id="quote-box">
+          <div className="quote-text">{text}</div>
+          <div className="quote-author">{author}</div>
+          <div className="buttons">
+            <button className="button" id="new-quote" onClick={fetchQ}>
+              Click
+            </button>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
+};
 
-    }
 export default App;
